a2b5452ac9298f87121ce7323ae9f5cff73cb273
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-05-06 20:12:31 -0300

    Add files via upload

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,11 @@
+import './Content.css'
+import React from 'react'
+
+
+const Content = props => (
+    <content className="Content">
+        <h1>Componente Content</h1>
+    </content>
+)
+
+export default Content
