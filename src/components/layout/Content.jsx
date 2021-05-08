d10cc3ebf04dc50b99ce89a92783378186d893fb
Luiz Henrique <58401291+MogLuiz@users.noreply.github.com>
2021-05-08 10:03:57 -0300

    Add files via upload

--- src/components/layout/Content.jsx
+++ src/components/layout/Content.jsx
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom'
 
 import Home from '../../views/examples/Home'
 import About from '../../views/examples/About'
+import Param from '../../views/examples/Param'
+import NotFound from '../../views/examples/NotFound'
 
 
 const Content = props => (
@@ -12,8 +14,14 @@ const Content = props => (
             <Route path="/about">
                 <About />
             </Route>
-            <Route path="/home">
+            <Route path="/param/:id">
+                <Param />
+            </Route>
+            <Route exact path="/">
                 <Home />
+            </Route>
+            <Route path="*">
+                <NotFound />
             </Route>
         </Switch>
     </content>
